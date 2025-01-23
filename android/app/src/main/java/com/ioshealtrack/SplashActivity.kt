2e9663f5615e67c215fa8f21ce68dc9b7f4bed82
OrianSm <surajmujawar2002@gmail.com>
2025-01-23 11:14:27 +0530

    V5.3.3

---
+++ android/app/src/main/java/com/ioshealtrack/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.ioshealtrack
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.launch_screen)
+
+        // Add a delay to show the splash screen
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 2000) // 2 seconds delay
+    }
+}

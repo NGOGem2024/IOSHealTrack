3ef695ca7772bd627c798c538dcda71a636f3454
Laharika Gudur <gudurlaharika@gmail.com>
2025-04-16 10:47:22 +0530

    changes pull

--- src/screens/AllAppointmen.tsx
+++ src/screens/AllAppointmen.tsx
@@ -195,7 +195,7 @@ const AllAppointmentsPage: React.FC<Props> = ({navigation}) => {
       // Convert the appointments object into an array of DayData
       const dayDataArray: DayData[] = [];
       const appointments = response.data.appointments;
-
+     // console.log(appointments)
       // Create a date iterator to ensure we have entries for all dates
       let currentDate = new Date(startDate);
       const endDateObj = new Date(endDate);

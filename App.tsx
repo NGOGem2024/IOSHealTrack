a674ccdbfcd15bfe88a8c83f163b2997a2504c2c
OrianSm <surajmujawar2002@gmail.com>
2025-05-20 15:42:08 +0530

    Removed text auto sizing

--- App.tsx
+++ App.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
+import {Text, TextInput} from 'react-native';
 import {ThemeProvider} from './src/screens/ThemeContext';
 import {SessionProvider} from './src/context/SessionContext';
 import AppNavigator from './AppNavigatior';
 import Toast from 'react-native-toast-message';
 
+// Disable font scaling globally for Text and TextInput (TypeScript-safe way)
+(Text as any).defaultProps = (Text as any).defaultProps || {};
+(Text as any).defaultProps.allowFontScaling = false;
+
+(TextInput as any).defaultProps = (TextInput as any).defaultProps || {};
+(TextInput as any).defaultProps.allowFontScaling = false;
+
 const App: React.FC = () => (
   <SessionProvider>
     <ThemeProvider>
       <AppNavigator />
+      <Toast />
     </ThemeProvider>
-    <Toast />
   </SessionProvider>
 );
 

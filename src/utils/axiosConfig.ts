7ede758445bf439765555fd31a17ab7c302018dd
OrianSm <surajmujawar2002@gmail.com>
2025-09-15 12:35:38 +0530

    Problem resolved

--- src/utils/axiosConfig.ts
+++ src/utils/axiosConfig.ts
@@ -13,8 +13,8 @@ NetInfo.addEventListener(state => {
 });
 
 const instance = axios.create({
-  baseURL: 'https://healtrack.azurewebsites.net/',
-  //baseURL: 'http://192.168.37.43:5000',
+  //baseURL: 'https://healtrack.azurewebsites.net/',
+  baseURL: 'http://192.168.31.171:5000',
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',

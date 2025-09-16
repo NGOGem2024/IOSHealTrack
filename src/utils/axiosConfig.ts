3b97bac8b0d0627f82808318deaa778dc9a42247
OrianSm <surajmujawar2002@gmail.com>
2025-09-16 15:34:55 +0530

    Wanting to update

--- src/utils/axiosConfig.ts
+++ src/utils/axiosConfig.ts
@@ -14,7 +14,7 @@ NetInfo.addEventListener(state => {
 
 const instance = axios.create({
   //baseURL: 'https://healtrack.azurewebsites.net/',
-  baseURL: 'http://192.168.31.171:5000',
+  baseURL: 'http://192.168.1.24:5000',
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
